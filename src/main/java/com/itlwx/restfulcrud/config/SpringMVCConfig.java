9a874266a773058abd3734aab3ef1beb42363e17
itlwx <721759983@qq.com>
2018-11-17 18:02:11 +0800

    i18n

--- src/main/java/com/itlwx/restfulcrud/config/SpringMVCConfig.java
+++ src/main/java/com/itlwx/restfulcrud/config/SpringMVCConfig.java
@@ -1,15 +1,9 @@
 package com.itlwx.restfulcrud.config;
 
-import org.springframework.beans.factory.ListableBeanFactory;
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.boot.autoconfigure.web.ResourceProperties;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 @Configuration
 public class SpringMVCConfig implements WebMvcConfigurer {
