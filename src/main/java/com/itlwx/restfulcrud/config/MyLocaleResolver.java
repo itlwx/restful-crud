8a57f1913e4a5f174f74693116d544028f95630b
itlwx <721759983@qq.com>
2018-11-18 14:28:11 +0800

    国际化

---
+++ src/main/java/com/itlwx/restfulcrud/config/MyLocaleResolver.java
@@ -0,0 +1,32 @@
+package com.itlwx.restfulcrud.config;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+public class MyLocaleResolver implements LocaleResolver {
+    @Override
+    public Locale resolveLocale(HttpServletRequest request) {
+        String lang = request.getParameter("lang");
+        Locale locale = null;
+        if(StringUtils.hasText(lang)){
+            //若连接中有指定语言就使用指定的语言
+            String[] s = lang.split("_");
+            locale = new Locale(s[0],s[1]);
+        }else{
+            //若没有就根据请求头来确定语言
+            String header = request.getHeader("Accept-Language");
+            String[] countryAndLang = header.substring(0, header.indexOf(",")).split("-");
+            locale = new Locale(countryAndLang[0],countryAndLang[1]);
+        }
+        return locale;
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Locale locale) {
+
+    }
+}
--- src/main/java/com/itlwx/restfulcrud/config/SpringMVCConfig.java
+++ src/main/java/com/itlwx/restfulcrud/config/SpringMVCConfig.java
@@ -1,6 +1,8 @@
 package com.itlwx.restfulcrud.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +16,12 @@ public class SpringMVCConfig implements WebMvcConfigurer {
         registry.addViewController("/index").setViewName("login.html");
         registry.addViewController("/index.html").setViewName("login.html");
         registry.addViewController("/").setViewName("login.html");
+        registry.addViewController("login.html").setViewName("login.html");
+    }
+
+    /*将自定义的区域信息组件添加到容器*/
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
     }
 }
