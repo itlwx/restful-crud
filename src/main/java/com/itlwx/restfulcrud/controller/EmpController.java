27b25f4c3dca968c795e6c59a78849d67f9b10c2
itlwx <721759983@qq.com>
2018-11-21 17:55:48 +0800

    添加编辑

--- src/main/java/com/itlwx/restfulcrud/controller/EmpController.java
+++ src/main/java/com/itlwx/restfulcrud/controller/EmpController.java
@@ -7,7 +7,7 @@ import com.itlwx.restfulcrud.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -27,9 +27,33 @@ public class EmpController {
     }
 
     @GetMapping(value="/emp")
-    public String add(Model model){
+    public String addPage(Model model){
         Collection<Department> departments = departmentDao.getDepartments();
         model.addAttribute("depts",departments);
         return "emp/add";
+    }
+
+    @PostMapping(value="/emp")
+    public String add(Employee emp){
+        employeeDao.save(emp);
+        //重定向到列表页面
+        return "redirect:/emps";
+    }
+
+    @GetMapping(value="/emp/{id}")
+    public String editPage(@PathVariable Integer id, Model model){
+        Employee employee = employeeDao.get(id);
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("emp",employee);
+        model.addAttribute("depts",departments);
+
+        /*添加页面与编辑页面二合一*/
+        return "emp/add";
+    }
+
+    @PutMapping(value="/emp")
+    public String edit(Employee emp){
+        employeeDao.save(emp);
+        return "redirect:/emps";
     }
 }
